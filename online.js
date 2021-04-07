8fa488abd5b625f82f44175d6657e884ef4b947a
Vérton <80917036+u0m@users.noreply.github.com>
2021-04-07 05:37:55 +0300

    Create online.js

---
+++ online.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js")
+, verton = new Discord.Client(); 
+verton.login("")
+const june1 = new Discord.Client(); 
+verton1.login("")
+const june2 = new Discord.Client(); 
+verton2.login("")
+const june3 = new Discord.Client(); 
+verton3.login("")
+const june4 = new Discord.Client(); 
+verton4.login("")
+const june5 = new Discord.Client(); 
+verton5.login("")
+const june6 = new Discord.Client(); 
+verton6.login("")
+const june7 = new Discord.Client(); 
+verton7.login("")
+const june8 = new Discord.Client(); 
+verton8.login("")
+const june9 = new Discord.Client(); 
+verton9.login("")
+const june10 = new Discord.Client(); 
+verton10.login("")
+const june11 = new Discord.Client(); 
+verton11.login("")
+const june12 = new Discord.Client(); 
+verton12.login("")
+const june13 = new Discord.Client(); 
+verton13.login("")
+const june14 = new Discord.Client(); 
+verton14.login("")
+const june15 = new Discord.Client(); 
+verton15.login("")
+const june16 = new Discord.Client(); 
+verton16.login("")
+const june17 = new Discord.Client(); 
+verton17.login("")
+const june18 = new Discord.Client(); 
+verton18.login("")
+const june19 = new Discord.Client(); 
+verton19.login("")
+const june20 = new Discord.Client(); 
+verton20.login("")
